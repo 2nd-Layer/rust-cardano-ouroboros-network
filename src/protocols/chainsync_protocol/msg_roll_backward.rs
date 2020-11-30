5639f6a335ffdcb1b48022aee89cbb38bc4fff20
Mark Stopka <mark.stopka@perlur.cloud>
2020-11-30 14:12:41 +0100

    Add attribution to prior work and copyright notice

--- src/protocols/chainsync_protocol/msg_roll_backward.rs
+++ src/protocols/chainsync_protocol/msg_roll_backward.rs
@@ -1,3 +1,14 @@
+/**
+Forked-off from https://github.com/AndrewWestberg/cncli/ on 2020-11-30
+© 2020 Andrew Westberg licensed under Apache-2.0
+
+Re-licensed under GPLv3 or LGPLv3
+© 2020 PERLUR Group
+
+SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
+
+*/
+
 use log::error;
 use serde_cbor::Value;
 
