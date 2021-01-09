a719dd4dcc9514d8082fce9d3382267d52126ca1
Mark Stopka <mark.stopka@perlur.cloud>
2021-01-09 06:42:58 +0100

    refactor: basic code

--- .github/tests/00_test_examples_agains_cardano_node.sh
+++ .github/tests/00_test_examples_agains_cardano_node.sh
@@ -25,44 +25,45 @@ echo "Pull Docker image from Docker Hub"
 if ! docker pull 2ndlayer/centos-cardano-node:${cardanoNodeVersion} >> /dev/null 2>&1; then
   echo "ERROR: Docker image pull failed!"
   exit 0
-else
-  TESTNET_MAGIC=${RANDOM}
+elif TESTNET_MAGIC=${RANDOM}; then
   if docker run 2ndlayer/centos-cardano-node:${cardanoNodeVersion} \
-      cardano-cli genesis create \
-        --genesis-dir testnet \
-        --gen-utxo-keys 3 \
-        --supply 100000 \
-        --testnet-magic ${TESTNET_MAGIC}; 
+    cardano-cli genesis create \
+      --genesis-dir testnet \
+      --gen-utxo-keys 3 \
+      --supply 100000 \
+      --testnet-magic ${TESTNET_MAGIC};
     then
       if IMAGE_ID=$(docker ps -aq | head -n 1); then
         if docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}; then
           echo "INFO: Initial testing Shelley environment created!"
+          if docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}; then
+            echo "INFO: New testing Shelley environment created!"
+            docker \
+              run local/cardano-node-shelley-testnet:${cardanoNodeVersion} \
+              bash <<< "cat testnet/genesis.spec.json | jq .epochLength=300 > testnet/genesis.spec.json.tmp"
+            IMAGE_ID=$(docker ps -aq | head -n 1)
+            docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}
+            docker \
+                run local/cardano-node-shelley-testnet:${cardanoNodeVersion} \
+                bash <<< "cat testnet/genesis.spec.json \
+                | jq .protocolParams.decentralisationParam=0 > testnet/genesis.spec.json.tmp"
+            IMAGE_ID=$(docker ps -aq | head -n 1)
+            docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}
+            docker \
+              run local/cardano-node-shelley-testnet:${cardanoNodeVersion} \
+              bash <<< "mv testnet/genesis.spec.json.tmp testnet/genesis.spec.json"
+            IMAGE_ID=$(docker ps -aq | head -n 1)
+            docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}
+            echo "DEBUG: Image name: local/cardano-node-shelley-testnet:${cardanoNodeVersion}"
+          fi
         fi
-      if docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}; then
-        echo "INFO: New testing Shelley environment created!"
-        docker \
-          run local/cardano-node-shelley-testnet:${cardanoNodeVersion} \
-          bash <<< "cat testnet/genesis.spec.json | jq .epochLength=300 > testnet/genesis.spec.json.tmp"
-        IMAGE_ID=$(docker ps -aq | head -n 1)
-        docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}
-        docker \
-            run local/cardano-node-shelley-testnet:${cardanoNodeVersion} \
-            bash <<< "cat testnet/genesis.spec.json \
-             | jq .protocolParams.decentralisationParam=0 > testnet/genesis.spec.json.tmp"
-        IMAGE_ID=$(docker ps -aq | head -n 1)
-        docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}
-        docker \
-          run local/cardano-node-shelley-testnet:${cardanoNodeVersion} \
-          bash <<< "mv testnet/genesis.spec.json.tmp testnet/genesis.spec.json"
-        IMAGE_ID=$(docker ps -aq | head -n 1)
-        docker commit ${IMAGE_ID} local/cardano-node-shelley-testnet:${cardanoNodeVersion}
-        echo "DEBUG: Image name: local/cardano-node-shelley-testnet:${cardanoNodeVersion}"
-      fi
       else
         echo "ERROR: Failed to create testing Shelley image!"
       fi
     else
-      echo "ERROR: Failed to create testing Shelley environment!"
+      echo "ERROR: Failed to obtain IMAGE ID testing Shelley environment!"
       exit 0
   fi
+else
+  echo "ERROR: Failed to create Shellet testing environment!"
 fi
